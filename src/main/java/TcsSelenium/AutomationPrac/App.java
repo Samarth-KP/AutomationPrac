f473933128d98bad6cc912b5b72b75c46c72da0d
Administrator <Administrator@172.31.2.255>
2024-01-31 05:29:29 +0000

    unwanted files

---
+++ src/main/java/TcsSelenium/AutomationPrac/App.java
@@ -0,0 +1,13 @@
+package TcsSelenium.AutomationPrac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
